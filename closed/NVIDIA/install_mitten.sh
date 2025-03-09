feb71af146d113eff64cfe7f743b44eb910d13c0
Shobhit <shobhitv@nvidia.com>
2025-03-08 23:46:22 -0800

    add dependancy build steps

--- closed/NVIDIA/install_mitten.sh
+++ closed/NVIDIA/install_mitten.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+# install typeguard
+pip install typeguard
+
+# install tritonclient
+pip install tritonclient[all]
+
+# install mitten
 cd /work/build/
 rm -rf mitten && git clone https://github.com/NVIDIA/mitten.git
 cd mitten
 git submodule update --init
 sed -i 's/numpy >=1.22.0, <1.24.0/numpy >=1.26.4/' ./setup.cfg
 pip install .
+
+# install cmake
+apt update
+apt install -y software-properties-common lsb-release
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+apt install -y cmake
+
+# install loadgen
+make build_loadgen
