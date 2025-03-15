8ba7c212df2d49be753b2ced5fa9e32f034c2732
zihaok <161090975+zihaok@users.noreply.github.com>
2025-03-15 11:36:49 +0800

    Update install_deps.sh

--- closed/NVIDIA/install_deps.sh
+++ closed/NVIDIA/install_deps.sh
@@ -16,9 +16,9 @@ pip install .
 
 # install cmake
 apt update
-apt install -y software-properties-common lsb-release
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+# apt install -y software-properties-common lsb-release
+# wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+# apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
 apt install -y cmake
 
 # install loadgen
